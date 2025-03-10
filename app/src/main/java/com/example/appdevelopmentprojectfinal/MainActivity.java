c9b391e62f288fa9276c4b9b466a029fbfddf5ac
Nur_Alislam_Kastiro <22331549@studentmail.ul.ie>
2025-03-10 12:47:37 +0000

    Navigation fragments for Homepage, Profile, Store and Timetable.

--- app/src/main/java/com/example/appdevelopmentprojectfinal/MainActivity.java
+++ app/src/main/java/com/example/appdevelopmentprojectfinal/MainActivity.java
@@ -7,18 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.appdevelopmentprojectfinal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFramgent(HomepageFragment());
+                    break;
+                case R.id.market:
+                    replaceFramgent(StoreFragment());
+                    break;
+                case R.id.timetable:
+                    replaceFramgent(TimetableFragment());
+                    break;
+                case R.id.profile:
+                    replaceFramgent(ProfileFragment());
+                    break;
+            }
+            return true;
         });
+    }
+
+    private void replaceFramgent (Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
