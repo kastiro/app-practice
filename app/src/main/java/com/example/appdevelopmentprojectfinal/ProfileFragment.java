ee96f8388aaa1cda057173aaf0b7a4c74f541344
Mykola Vaskevych <22372199@studentmail.ul.ie>
2025-03-27 23:07:42 +0000

    - Renamed generic mParam variables in fragments to meaningful names - Updated Store and Profile fragment layouts to display text properly - Added toString() methods to data model classes - Improved error handling in JSON loading - Separated onCreateView and onViewCreated in TimetableFragment - Added proper annotations for better type safety - General code cleanup and readability improvements - fixed some warnings as idle suggested

--- app/src/main/java/com/example/appdevelopmentprojectfinal/ProfileFragment.java
+++ app/src/main/java/com/example/appdevelopmentprojectfinal/ProfileFragment.java
@@ -15,14 +15,9 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // These parameters can be used to pass data to the fragment if needed
+    private static final String ARG_USER_ID = "userId";
+    private static final String ARG_USER_NAME = "userName";
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -32,16 +27,15 @@ public class ProfileFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
+     * @param userId The user ID to display profile for
+     * @param userName The user name to display
+     * @return A new instance of fragment ProfileFragment
      */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance(String userId, String userName) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_USER_ID, userId);
+        args.putString(ARG_USER_NAME, userName);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,10 +43,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Arguments processing can be added here if needed in the future
     }
 
     @Override
